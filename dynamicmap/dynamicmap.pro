6407531320d6079098ca3c4d63dcc9896546775e
Kirill Fertikov <kirill.fertikov@gmail.com>
2014-03-30 23:36:32 +0400

    gui started

---
+++ dynamicmap/dynamicmap.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-03-30T14:01:46
+#
+#-------------------------------------------------
+
+QT       += core gui webkit
+
+TARGET = dynamicmap
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    logindialog.cpp
+
+HEADERS  += mainwindow.h \
+    logindialog.h
+
+FORMS    += mainwindow.ui \
+    logindialog.ui
---
+++ dynamicmap/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "logindialog.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+public slots:
+    void openLoginDialog();
+    
+private:
+    Ui::MainWindow *ui;
+    LoginDialog *loginDialog;
+};
+
+#endif // MAINWINDOW_H
